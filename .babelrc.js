e34ea8c83d5975ee45205d90231b624acf8e2fc9
JuLong <julong1988@naver.com>
2021-09-18 17:40:53 +0900

    fix(common): test 환경 추가

---
+++ .babelrc.js
@@ -0,0 +1,13 @@
+const config = {
+  presets: [
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+    "@babel/preset-env",
+  ],
+  plugins: [
+    // "@babel/plugin-transform-typescript",
+    // "babel-plugin-jsx-remove-data-test-id"
+  ]
+};
+
+module.exports = config;
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  roots: [
+    '<rootDir>/src'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest/fileMock.js',
+    '\\.(css|less|styl)$': `<rootDir>../node_modules/identity-obj-proxy`,
+    "@/(.*)":  "<rootDir>/src/$1",
+  },
+  setupFiles: ['<rootDir>/jest/setupFiles.js'],
+};
