7cf251ad1ff46f4ebb1bfb4323a181d108d05cae
JuLong <julong@wemakeprice.com>
2021-12-22 14:18:30 +0900

    fix: 라우터 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
-import Main from '@/components/main';
+import Loader from './Loader';
 
 // const routes = window.VAR_ROUTES;
 // console.log(routes);
+const Apage = Loader('aaa');
+const DashboardPage = Loader('dashboard');
+const FormPage = Loader('form');
+const NetworkPage = Loader('network');
+const SamplePage = Loader('sample');
+const QueryPage = Loader('query');
 
 const Router: React.FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="*" element={<Main />}></Route>
+      <Route index element={<DashboardPage />} />
+      <Route path="dashboard" element={<DashboardPage />} />
+      <Route path="form" element={<FormPage />} />
+      <Route path="network" element={<NetworkPage />} />
+      <Route path="sample" element={<SamplePage />} />
+      <Route path="query" element={<QueryPage />} />
+      <Route path="aaa" element={<Apage />} />
     </Routes>
   </BrowserRouter>
 );
